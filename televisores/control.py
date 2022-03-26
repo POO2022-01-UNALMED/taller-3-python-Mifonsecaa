46a5dfd5d897be4df9567d090a0de9a34ff2235a
Mifonsecaa <92172868+Mifonsecaa@users.noreply.github.com>
2022-03-26 15:57:36 -0500

    Create control.py

---
+++ televisores/control.py
@@ -0,0 +1,36 @@
+from televisores.tv import TV
+
+class Control:
+    def __init__(self, tv):
+        self._tv = tv
+    
+    def turnOn(self):
+        self.turnOn = TV.turnON
+
+    def turnOff(self):
+        self.turnOff = TV.turnOff
+
+    def canalUp(self):
+        self.canalUp = TV.canalUp
+
+    def canalDown(self):
+        self.canalDown = TV.canalDown
+
+    def volumenUp(self):
+        self.volumenUp = TV.volumenUp
+
+    def volumenDown(self):
+        self.volumenDown = TV.volumenDown
+    
+    def setCanal(self):
+        self.setCanal = TV.setCanal
+
+    def enlazar(self, televisor):
+        self.settv = televisor
+        TV.setControl = self.settv
+
+    def settv(self, televisor):
+        self._tv = televisor
+    
+    def gettv(self):
+        return self._tv
